e2df0ea08dad7fcfc0501826f95fa88316cc0994
CAAMCODE <ing.amaya.bautista@gmail.com>
2022-05-28 23:53:48 -0500

    28 de mayo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -130,9 +130,9 @@ function esPar(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (num % 2 == 0) {
-    true;
+    return true;
   } else {
-    false;
+    return false;
   }
 }
 
@@ -141,9 +141,9 @@ function esImpar(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (num % 2 == 1) {
-    true;
+    return true;
   } else {
-    false;
+    return false;
   }
 }
 
@@ -151,31 +151,44 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+
+  var resultado= Math.pow(num,2)
+  return resultado;
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  var resultado= Math.pow(num,3)
+  return resultado;
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
+  var resultado= Math.pow(num,exponent)
+  return resultado;
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
+  var resultado= Math.round(num)
+  return resultado;
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  var resultado= Math.ceil(num)
+  return resultado;
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
+  var num1 = Math.random();
+  return num1;
 }
 
 function esPositivo(numero) {
@@ -183,45 +196,72 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+
+  if (numero>0){
+     var String1 ="Es positivo"
+     return String1
+
+  }else if(numero<0){
+    var String2 ="Es negativo"
+     return String2
+
+  }else {
+    return false
+  }
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  const strin1= str+"!"
+  return strin1
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  const strin1= nombre+" "+apellido
+  return strin1
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  const strin1= "Hola "+nombre+"!"
+  return strin1
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  const area= alto* ancho
+  return area
 }
 
 function retornarPerimetro(lado) {
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+  const perimetro= 4*lado
+  return perimetro
 }
 
 function areaDelTriangulo(base, altura) {
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+
+  const areat= (base*altura)/2
+  return areat
 }
 
 function deEuroAdolar(euro) {
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  const convierte = euro*1.20
+  return convierte
 }
 
 function esVocal(letra) {
@@ -230,6 +270,22 @@ function esVocal(letra) {
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  const vocales = "aáeéiíoóuú";
+  const letraminuscula =letra.toLowerCase()
+  if (letra.length>1){
+    const strin1="Dato incorrecto"
+    return strin1
+  }else if(isNaN(letra)){
+    const strin2="Dato incorrecto"
+    return strin2
+
+  }else if (vocales.includes(letra.toLowerCase())) {
+
+    const strin3="Es vocal"
+    return strin3
+    
+}
+
 }
 
 // No modificar nada debajo de esta línea
